858a380f1e76d8a9791ef2add84fdbae6a9c651a
Abdul samad <abdulsmdpm786@gmail.com>
2026-07-22 20:56:56 +0530

    changes for message

--- Server/Routes/v2/index.js
+++ Server/Routes/v2/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import listingRoutes from "./listingRoutes.js"
 import messageRoutes from "./messageRoute.js"
+import adminMessageRoutes from "./adminMessageRoutes.js"
 
 const v2Routes = express.Router()
 
